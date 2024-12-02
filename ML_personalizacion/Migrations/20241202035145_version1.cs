0fedfd6790d056dec5eceb702e5d30ba2a1a3632
Matias Eduardo Lopez Peña <matias.lopez@udla.edu.ec>
2024-12-01 22:58:51 -0500

    creacion de api

---
+++ ML_personalizacion/Migrations/20241202035145_version1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ML_personalizacion.Migrations
+{
+    /// <inheritdoc />
+    public partial class version1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ML_personas",
+                columns: table => new
+                {
+                    ML_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ML_nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ML_apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ML_email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ML_telefono = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ML_personas", x => x.ML_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ML_personas");
+        }
+    }
+}
