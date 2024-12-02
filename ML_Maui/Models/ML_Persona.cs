1aa9d817c747fe0c913ed93dbb707a60347cecbb
Matias Eduardo Lopez Peña <matias.lopez@udla.edu.ec>
2024-12-02 00:07:29 -0500

    definicion de DTO

---
+++ ML_Maui/Models/ML_Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML_Maui.Models
+{
+    public class ML_Persona
+    {
+        public int mL_id { get; set; }
+        public string mL_nombre { get; set; }
+        public string mL_apellido { get; set; }
+        public string mL_email { get; set; }
+        public string mL_telefono { get; set; }
+    }
+
+
+}
